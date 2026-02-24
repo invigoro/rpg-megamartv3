243d56b3300f63e228012df2c45102d6485e5bd4
Timothy Wells <35319035+invigoro@users.noreply.github.com>
2026-02-24 11:35:32 -0700

    refactor

--- scripts/itemRenderer.js
+++ scripts/itemRenderer.js
@@ -4,22 +4,7 @@ function renderItems(data) {
         for (i = 0; i < dataIs.length; i++) {
             let item = dataIs[i];
             console.log(item);
-            $("#container-row-1").append(`<div class='col-xl-3 col-lg-4 col-md-6 col-12'>
-                <div class='h-100 py-3 px-1'>
-                    <div class='h-100 py-2 px-3 border border-info rounded bg-light'>
-                <p class='h3 text-muted'>${item.title}</p>
-                <div class='text-secondary'>
-                <p style='font-weight: 100; font-style: italic;'>${item.overview}</p>
-                <div class='row'>
-                ${item.quantity > 0 ? `<div class='col'><p class='text-start'>Cost: ${item.cost} gp</p></div>` : "" }
-                <div class='col'><p class='text-end'>${item.quantity > 0 ? "Available: " + item.quantity : "<span class='text-danger text-bold'>Out of stock!</span>"}</p></div>
-                </div>
-                <div style='height: 200px'><img class='rounded mx-auto d-block' style='max-width: 200px;max-height: 200px' src='${item.img}' onerror="this.onerror=null; this.src='./images/default.png'"></div>
-                <p>${item.description}</p>
-                </div>
-                </div>
-                </div>
-            `);
+            $("#container-row-1").append(formatItem(item));
         }
         dataOoS = data.filter(item => item.visible && item.quantity <= 0);
         if(dataOoS.length > 0)
@@ -27,7 +12,12 @@ function renderItems(data) {
         for (i = 0; i < dataOoS.length; i++) {
             let item = dataOoS[i];
             console.log(item);
-            $("#container-row-2").append(`<div class='col-xl-3 col-lg-4 col-md-6 col-12'>
+            $("#container-row-2").append(formatItem(item));
+        }
+}
+
+function formatItem(item) {
+    return `<div class='col-xl-3 col-lg-4 col-md-6 col-12'>
                 <div class='h-100 py-3 px-1'>
                     <div class='h-100 py-2 px-3 border border-info rounded bg-light'>
                 <p class='h3 text-muted'>${item.title}</p>
@@ -42,8 +32,7 @@ function renderItems(data) {
                 </div>
                 </div>
                 </div>
-            `);
-        }
+            `;
 }
 
 
